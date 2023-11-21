bf29d1860893948959ba6d42fa56deef0e0aca6d
dkjonzy2 <dkjonzy2@byu.edu>
2023-11-21 00:32:47 -0700

    publish

---
+++ Birthday Scraper.py
@@ -0,0 +1,76 @@
+# Birthday Scraper
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+
+path_to_chromedriver = '/Users/danjones/Downloads/chrome-mac-arm64' # change path as needed
+ACCOUNT = "dan.jones"
+PASSWORD = "1p2o3i1p2o3I"
+
+
+driver = webdriver.Chrome()
+
+wait = WebDriverWait(driver, 30)
+driver.get("https://lcr.churchofjesuschrist.org/report/birthday-list?lang=eng")
+
+wait.until(EC.element_to_be_clickable((By.NAME, "username"))).send_keys(ACCOUNT)
+time.sleep(1)
+wait.until(EC.element_to_be_clickable((By.ID, "okta-signin-submit"))).click()
+time.sleep(1)
+wait.until(EC.element_to_be_clickable((By.NAME, "password"))).send_keys(PASSWORD)
+time.sleep(1)
+wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@value='Verify']"))).click()
+print("Logged in")
+
+wait.until(EC.visibility_of_element_located((By.ID, "menuItem7")))
+print(driver.title)
+driver.get("https://lcr.churchofjesuschrist.org/report/birthday-list?lang=eng")
+print(driver.title)
+
+# Locate the select element
+select_element = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((By.XPATH, "(//div[@class='sc-xne1im-0 kWhksG']//select[@class='sc-11vq36o-0 dvAIOs'])[3]"))
+)
+
+# Create a Select object
+select = Select(select_element)
+
+# Select an option by its value (e.g., to show 3 months)
+select.select_by_value('12')
+
+# Wait for the table to load
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "tr.sc-zkgtp5-0.iooGpB.sc-bee01cd9-2.jbMyIm")))
+
+# Get all the rows
+rows = driver.find_elements(By.CSS_SELECTOR, "tr.sc-zkgtp5-0.iooGpB.sc-bee01cd9-2.jbMyIm")
+print("Found {} rows".format(len(rows)))
+# Create an empty list to hold the data
+data = []
+
+# Iterate through each row
+for row in rows:
+    # Get all the columns in the row
+    cols = row.find_elements(By.TAG_NAME, "td")
+    
+    # Extract the text from each column
+    date = cols[0].text
+    name = cols[1].text
+    age = cols[2].text
+    phone = cols[3].text
+    address = cols[4].text
+    
+    # Append the data to the list
+    data.append([date, name, age, phone, address])
+
+# Create a DataFrame from the list
+df = pd.DataFrame(data, columns=["Date", "Name", "Age", "Phone", "Address"])
+
+# Export the DataFrame to a CSV file
+df.to_csv("birthdays.csv", index=False)
+print("Exported data to CSV file")
+print("Done")
---
+++ slack_bot.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from datetime import datetime, timedelta
+import pytz
+from slack_sdk import WebClient
+import os
+import requests
+import logging
+
+#TODO: Post an update Sunday night with the upcoming birthdays for the week
+#TODO: Automatically update it's own birthday list by scraping the website
+#TODO: write with customized appearance
+#TODO: react to everything in the channel with emojis
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+class GPTError(Exception):
+    pass
+
+PROMPT = """Today is {date}. {first_name} {last_name} is turning a year older. 
+Wish them a happy birthday! Keep your response short and sweet. Don't include their 
+age in the message in case they are sensitive about it. You post a birthday message 
+few days so make each message unique. Do things like include emojis, 
+write a short poem, or make a joke."""
+
+def get_gpt_message(first_name, last_name, date):
+    prompt = PROMPT.format(date=date.strftime('%B %d'), first_name=first_name, last_name=last_name)
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}"
+    }
+    data = {
+        "model": "gpt-4",
+        "messages": [{"role": "user", "content": prompt}]
+    }
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code != 200:
+        logging.error(f"Error with OpenAI API: {response.status_code} - {response.text}")
+        raise GPTError("Error with OpenAI API")
+    return response.json()['choices'][0]['message']['content']
+
+def send_slack_message(client, channel, text):
+    client.chat_postMessage(channel=channel, text=text)
+
+def prepare_data():
+    data = pd.read_csv('birthdays.csv', parse_dates=['Date'], dayfirst=True)
+    data['Date'] = pd.to_datetime(data['Date'] + ' ' + str(datetime.now().year), format='%d %b %Y')
+    data[['Last Name', 'First Name']] = data['Name'].str.split(', ', expand=True) #TODO: this leaves middle names attached to first names
+    return data
+
+def check_for_birthdays(date=None):
+    data = prepare_data()
+    utah = pytz.timezone('America/Denver')
+    date = date or datetime.now(utah).date()
+    todays_birthdays = data[(data['Date'].dt.month == date.month) & (data['Date'].dt.day == date.day)]
+    
+    slack_token = os.environ['SLACK_BOT_TOKEN']
+    client = WebClient(token=slack_token)
+    channel = 'C03S8HDLWK1' if os.environ['env'] == 'production' else 'C0621KYQ09M'
+
+    for _, row in todays_birthdays.iterrows():
+        try:
+            message = get_gpt_message(row['First Name'], row['Last Name'], row['Date'])
+        except GPTError:
+            message = f"Happy Birthday to {row['First Name']} {row['Last Name']}! 🎂"
+            logging.error("Error in getting GPT message, using default message")
+        send_slack_message(client, channel, message)
+
+if __name__ == "__main__":
+    check_for_birthdays()
